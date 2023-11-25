575dac8eb9792366398a59e3989475c2126e095c
RoberioLeal2 <roberio.76leau@gmail.com>
2023-11-25 15:45:50 -0300

    Atualizando classe Softwre

---
+++ Software.java
@@ -0,0 +1,14 @@
+public class Software {
+   
+    String n_Software;
+  
+    public Software(String n_Software) {
+        this.n_Software = n_Software;
+    }
+
+    public String getn_Software() {
+        return this.n_Software;
+    }
+    
+    
+}
