41fe46fc8209c6bf9175f7b220214e35b2a69a76
ALVESROMULO <joao-romulo@bol.com.br>
2023-11-26 14:31:39 -0300

    Criando classe Ferramenta

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 
 public class Main {
     
+        Ferramenta ide = new Ferramenta("VSCode");
+        Ferramenta photoshop = new Ferramenta("AutoCad");
+        Ferramenta MOffice = new Ferramenta("Excel");
+        Ferramenta SGBDs = new Ferramenta("MySQL");
+        Ferramenta totvs = new Ferramenta("Financeiro");
+
+
     public static void calcularPorcentagem(Funcionario funcionario) {
         if (funcionario.getFerramenta() != null) {
             System.out.println("Cargo: " + funcionario.getCargo() + " - Ferramenta Escolhida: " + funcionario.getFerramenta().getNome()+ " - Ok");
