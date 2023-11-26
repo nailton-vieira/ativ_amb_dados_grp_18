410339d525f899211b24921529abd0953f005e3c
RoberioLeal2 <roberio.76leau@gmail.com>
2023-11-26 13:27:03 -0300

    Atualizando a classe main

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 
 public class Main {
+     Software n_gruposoft = new Software(" Atividade Final - Grupo 18 - Software House - v.1.0.1 ");
+        System.out.println("****** " + n_gruposoft.getn_Software()+ "******" + "\n");
+        System.out.println("<*****> Percentual de Contribuição por Profissional/Cargo no projeto Sofware House <*****>" + "\n");
+        
     
     public static void calcularPorcentagem(Funcionario funcionario) {
         if (funcionario.getFerramenta() != null) {
