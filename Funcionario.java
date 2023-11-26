bd7bdcaecf3b5d2770afeb1f5d197c381a3c10bc
nailton-vieira <nailtonvieira@gmail.com>
2023-11-26 09:35:18 -0200

    Atualizando a classe Funcionario

---
+++ Funcionario.java
@@ -0,0 +1,13 @@
+public class Funcionario{
+    
+    private String cargo;
+  
+
+    public Funcionario(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return this.cargo;
+    }
+}
