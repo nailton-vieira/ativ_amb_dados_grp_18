3b1be12d7421ec42edee4e050139200fcb2b418b
FalcaoUnifor <marcelo123falcao@unifor.br>
2023-11-26 16:12:18 -0300

    Criando a classe Demanda

---
+++ Demanda.java
@@ -0,0 +1,15 @@
+public class Demanda {
+
+    String nDemanda;
+
+    public Demanda(String nDemanda){
+     this.nDemanda = nDemanda;
+
+    }
+
+    public String getNDemanda(){
+    return this.nDemanda;
+
+    }
+
+}
